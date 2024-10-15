587d639f3cf4ee2c285bc798d4e3a38804476c9b
HelloWorldWin <143871992+EpsilonZYJ@users.noreply.github.com>
2024-10-15 20:22:00 +0800

    test: Add and Pass all move command test cases

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -53,4 +53,40 @@ namespace adas
         const Pose target({1, 0, 'E'});       // 如果执行M指令正确，新的姿势应该是target：{1， 0， 'E'}
         ASSERT_EQ(target, executor->Query()); // 当M指令执行完，executor->Query返回的汽车姿势应该等于target: {1， 0， 'E'}
     }
+    TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
+    {
+        // given 给定一个executor
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+
+        // when 调用executor的Execute方法去执行M指令
+        executor->Execute("M");
+
+        // then
+        const Pose target({-1, 0, 'W'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
+    {
+        // given 给定一个executor
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+
+        // when 调用executor的Execute方法去执行M指令
+        executor->Execute("M");
+
+        // then
+        const Pose target({0, 1, 'N'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
+    {
+        // given 给定一个executor
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+
+        // when 调用executor的Execute方法去执行M指令
+        executor->Execute("M");
+
+        // then
+        const Pose target({0, -1, 'S'});
+        ASSERT_EQ(target, executor->Query());
+    }
 }
