18bdd6dbe941d20a6d15f60000eb0ee0feaa8014
HelloWorldWin <143871992+EpsilonZYJ@users.noreply.github.com>
2024-11-05 20:36:43 +0800

    use lambda to optimize code

--- src/Command.hpp
+++ src/Command.hpp
@@ -1,57 +1,83 @@
 #pragma once
 
 #include "PoseHandler.hpp"
+#include <functional>
 
 namespace adas{
-    class ICommand
-    {
-    public:
-    virtual ~ICommand() = default;
-    virtual void DoOperate(PoseHandler& poseHandler) const noexcept = 0;
-    };
+    // class ICommand
+    // {
+    // public:
+    // virtual ~ICommand() = default;
+    // virtual void DoOperate(PoseHandler& poseHandler) const noexcept = 0;
+    // };
 
-    class MoveCommand final : public ICommand
+    class MoveCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler& poseHandler) const noexcept override
-        {
-            if(poseHandler.IsFast())
-            {
+        // void DoOperate(PoseHandler& poseHandler) const noexcept override
+        // {
+        //     if(poseHandler.IsFast())
+        //     {
+        //         poseHandler.Move();
+        //     }
+        //     poseHandler.Move();
+        // }
+        
+        // 定义函数对象operate，接收参数PoseHandler，返回void
+        const std::function<void(PoseHandler& PoseHandler)> operate = [](PoseHandler& poseHandler) noexcept{
+            if(poseHandler.IsFast()){
                 poseHandler.Move();
             }
             poseHandler.Move();
-        }
+        };
     };
-    class TurnLeftCommand final : public ICommand
+    class TurnLeftCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler& poseHandler) const noexcept override
-        {
-            if(poseHandler.IsFast())
-            {
+        // void DoOperate(PoseHandler& poseHandler) const noexcept override
+        // {
+        //     if(poseHandler.IsFast())
+        //     {
+        //         poseHandler.Move();
+        //     }
+        //     poseHandler.TurnLeft();
+        // }
+
+        const std::function<void(PoseHandler& PoseHandler)> operate = [](PoseHandler& poseHandler) noexcept{
+            if(poseHandler.IsFast()){
                 poseHandler.Move();
             }
             poseHandler.TurnLeft();
-        }
+        };
     };
-    class TurnRightCommand final : public ICommand
+    class TurnRightCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler& poseHandler) const noexcept override
-        {
-            if(poseHandler.IsFast())
-            {
+        // void DoOperate(PoseHandler& poseHandler) const noexcept override
+        // {
+        //     if(poseHandler.IsFast())
+        //     {
+        //         poseHandler.Move();
+        //     }
+        //     poseHandler.TurnRight();
+        // }
+        const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept{
+            if(poseHandler.IsFast()){
                 poseHandler.Move();
             }
             poseHandler.TurnRight();
-        }
+        };
     };
-    class FastCommand final : public ICommand
+
+    class FastCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler& poseHandler) const noexcept override
-        {
+        // void DoOperate(PoseHandler& poseHandler) const noexcept override
+        // {
+        //     poseHandler.Fast();
+        // }
+        const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept{
             poseHandler.Fast();
-        }
+        };
     };
 } // namespace adas
--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -26,15 +26,19 @@ namespace adas
     void ExecutorImpl::Execute(const std::string &commands) noexcept
     {
         //表驱动
-        std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
+        std::unordered_map<char, std::function<void(PoseHandler& PoseHandler)>> cmderMap;
         //建立操作M和前进指令的映射关系
-        cmderMap.emplace('M', std::make_unique<MoveCommand>());
+        MoveCommand moveCommand;
+        cmderMap.emplace('M', moveCommand.operate);
         //建立操作L和左转指令的映射关系
-        cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
+        TurnLeftCommand turnLeftCommand;
+        cmderMap.emplace('L', turnLeftCommand.operate);
         //建立操作R和右转指令的映射关系
-        cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
+        TurnRightCommand turnRightCommand;
+        cmderMap.emplace('R', turnRightCommand.operate);
         //建立操作F和快速指令的映射关系
-        cmderMap.emplace('F', std::make_unique<FastCommand>());
+        FastCommand fastCommand;
+        cmderMap.emplace('F', fastCommand.operate);
 
         // 遍历commands里面的每个指令cmd
         for (const auto cmd : commands)
@@ -43,7 +47,7 @@ namespace adas
             const auto it = cmderMap.find(cmd);
             // 如果找到表驱动，执行操作对应的指令
             if(it != cmderMap.end()){
-                it->second->DoOperate(poseHandler);
+                it->second(poseHandler);
             }
         }
     }
