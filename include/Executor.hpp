21e8363e8d30c13b77fbf5047eadfce89dee52f5
HelloWorldWin <143871992+EpsilonZYJ@users.noreply.github.com>
2024-11-12 20:59:08 +0800

    feat: Add test cases for Bus&SportCar

--- include/Executor.hpp
+++ include/Executor.hpp
@@ -10,6 +10,13 @@ namespace adas
         char heading; //"N" "S" "W" "E"代表四个方向
     };
 
+    /*车辆类型枚举*/
+    enum class ExecutorType{
+        NORMAL,
+        SPORTS_CAR,
+        BUS,
+    };
+
     /*
         驾驶动作执行接口
     */
@@ -17,7 +24,8 @@ namespace adas
     {
     public:
         // Caller should delete *excutor when it is no longer needed.
-        static Executor *NewExecutor(const Pose &pose = {0, 0, 'N'}) noexcept;
+        static Executor *NewExecutor(const Pose &pose = {0, 0, 'N'},
+            const ExecutorType ExecutorType = ExecutorType::NORMAL) noexcept;
 
     public:
         // 默认构造函数
